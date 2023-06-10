ce8455b870133bfba1f1794929d96b4a803b4e6c
CJ Walker <walkercj@microsoft.om>
2023-06-10 03:55:15 -0400

    Update Dist

--- dist/types/editabledetailslistprops.d.ts
+++ dist/types/editabledetailslistprops.d.ts
@@ -1,70 +1,53 @@
-import {
-  ConstrainMode,
-  IButtonStyles,
-  ICommandBarItemProps,
-  ICommandBarStyleProps,
-  ICommandBarStyles,
-  IDetailsListProps,
-  IScrollablePaneStyleProps,
-  IScrollablePaneStyles,
-  IStyleFunctionOrObject,
-} from "@fluentui/react";
+import { ConstrainMode, IButtonStyles, ICommandBarItemProps, ICommandBarStyleProps, ICommandBarStyles, IDetailsListProps, IScrollablePaneStyleProps, IScrollablePaneStyles, IStyleFunctionOrObject } from "@fluentui/react";
 import { IColumnConfig } from "./columnconfigtype";
 import { IGridCopy } from "./gridcopytype";
 import { IRowAddWithValues } from "./rowaddtype";
-export type IContentScrollablePaneStyleProps =
-  Partial<IScrollablePaneStyleProps> & {
+export type IContentScrollablePaneStyleProps = Partial<IScrollablePaneStyleProps> & {
     scrollablePaneOffsetHeight?: number;
     scrollablePaneContentOffsetHeight?: number;
-  };
+};
 export interface Props extends IDetailsListProps {
-  id: number;
-  actionIconStylesInGrid?: IButtonStyles;
-  items: any[];
-  columns: IColumnConfig[];
-  commandBarStyles?: IStyleFunctionOrObject<
-    ICommandBarStyleProps,
-    ICommandBarStyles
-  >;
-  enableCSVExport?: boolean;
-  enableExcelExport?: boolean;
-  exportFileName?: string;
-  enableSaveChangesOnlyOnSubmit?: boolean;
-  enableRowEditCopy?: boolean;
-  enableRowEditDelete?: boolean;
-  enableRowEdit?: boolean;
-  enableRowEditCancel?: boolean;
-  enableColumnEdit?: boolean;
-  enableBulkEdit?: boolean;
-  enableSingleCellEditOnDoubleClick?: boolean;
-  onGridSelectionChange?: any;
-  onGridUpdate?: any;
-  onGridSave?: any;
-  enableGridRowsDelete?: boolean;
-  enableGridRowsAdd?: boolean;
-  enableRowAddWithValues?: IRowAddWithValues;
-  enableEditMode?: boolean;
-  enableEditModeCancel?: boolean;
-  enablePagination?: boolean;
-  pageSize?: number;
-  onExcelExport?: any;
-  height?: string;
-  scrollablePaneStyles?: IStyleFunctionOrObject<
-    IContentScrollablePaneStyleProps,
-    IScrollablePaneStyles
-  >;
-  width?: string;
-  position?: string;
-  constrainMode?: ConstrainMode;
-  enableUnsavedEditIndicator?: boolean;
-  enableGridReset?: boolean;
-  enableColumnFilterRules?: boolean;
-  enableColumnFilters?: boolean;
-  enableCommandBar?: boolean;
-  enableSingleClickCellEdit?: boolean;
-  onGridStatusMessageCallback?: any;
-  gridCopyOptions?: IGridCopy;
-  enableDefaultEditMode?: boolean;
-  customCommandBarItems?: ICommandBarItemProps[];
-  customCommandBarOverflowItems?: ICommandBarItemProps[];
+    id: number;
+    actionIconStylesInGrid?: IButtonStyles;
+    items: any[];
+    columns: IColumnConfig[];
+    commandBarStyles?: IStyleFunctionOrObject<ICommandBarStyleProps, ICommandBarStyles>;
+    enableCSVExport?: boolean;
+    enableExcelExport?: boolean;
+    exportFileName?: string;
+    enableSaveChangesOnlyOnSubmit?: boolean;
+    enableRowEditCopy?: boolean;
+    enableRowEditDelete?: boolean;
+    enableRowEdit?: boolean;
+    enableRowEditCancel?: boolean;
+    enableColumnEdit?: boolean;
+    enableBulkEdit?: boolean;
+    enableSingleCellEditOnDoubleClick?: boolean;
+    onGridSelectionChange?: any;
+    onGridUpdate?: any;
+    onGridSave?: any;
+    enableGridRowsDelete?: boolean;
+    enableGridRowsAdd?: boolean;
+    enableRowAddWithValues?: IRowAddWithValues;
+    enableEditMode?: boolean;
+    enableEditModeCancel?: boolean;
+    enablePagination?: boolean;
+    pageSize?: number;
+    onExcelExport?: any;
+    height?: string | number;
+    scrollablePaneStyles?: IStyleFunctionOrObject<IContentScrollablePaneStyleProps, IScrollablePaneStyles>;
+    width?: string | number;
+    position?: string;
+    constrainMode?: ConstrainMode;
+    enableUnsavedEditIndicator?: boolean;
+    enableGridReset?: boolean;
+    enableColumnFilterRules?: boolean;
+    enableColumnFilters?: boolean;
+    enableCommandBar?: boolean;
+    enableSingleClickCellEdit?: boolean;
+    onGridStatusMessageCallback?: any;
+    gridCopyOptions?: IGridCopy;
+    enableDefaultEditMode?: boolean;
+    customCommandBarItems?: ICommandBarItemProps[];
+    customCommandBarOverflowItems?: ICommandBarItemProps[];
 }
